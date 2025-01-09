efff5b27179d42dfb324f4bb0e0a82181b51e193
Zdravko-jel <zdravko.zhel@gmail.com>
2025-01-09 15:57:24 +0200

    Using Floyed Warshall method

--- C.cpp
+++ C.cpp
@@ -1,20 +1,87 @@
-// C.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Floyed Warshall method
+
+vector<int> maxPathBetweenColors(int n, int numColors, vector<vector<int>>& adj, vector<int>& colors) {
+	vector<int> ans;
+	vector<vector<int>> matrix(n + 1, vector<int>(n + 1, -1));
+	for (int i = 0; i < adj.size(); i++)
+	{
+		matrix[i][i] = 0;
+
+		for (auto it: adj[i]) {
+			matrix[i][it] = 1;
+			matrix[it][i] = 1;
+		}
+	}
+
+	int n = matrix.size();
+
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				matrix[i][j] = (matrix[i][j] > matrix[i][k] + matrix[k][j]) ? matrix[i][j] : matrix[i][k] + matrix[k][j];
+			}
+		}
+	}
+
+	for (int i = 1; i < numColors + 1; i++)
+	{
+		int curr_sum = -1;
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = 0; k < n; k++)
+			{
+				if (matrix[j][k] > curr_sum && j != k && colors[j] == colors[k])
+				{
+					curr_sum = matrix[j][k];
+				}
+			}
+		}
+
+		ans.push_back(curr_sum);
+	}
+
+	return ans;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int n, k;
+		cin >> n >> k;
+
+		vector<vector<int>> adj(n+1);
+		vector<int> colors(n+1);
+
+		int a, b;
+		cin >> a >> b;
+
+		colors[1] = b;
+
+		for (int i = 1; i < n; i++) {
+			int parent, color;
+			cin >> parent >> color;
+
+			colors[i + 1] = color;
+			adj[parent].push_back(i + 1);
+			adj[i + 1].push_back(parent);
+		}
+
+		vector<int> res = maxPathBetweenColors(n, k, adj, colors);
+
+		for (int i = 0; i < res.size(); i++)
+		{
+			cout << res[i] << endl;
+		}
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
